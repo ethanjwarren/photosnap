b9a015009ed27474d618a12330c6c38f9cf4416a
Ethan Warren <ethan.warren10@gmail.com>
2022-10-04 12:13:41 +0100

    Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Photosnap</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">Stories</a>
+        <a href="/">Features</a>
+        <a href="/">Pricing</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

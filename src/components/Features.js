f7bb1e05a96ffc345a35a8cf0f66b1cbd95c7e49
Ethan Warren <ethan.warren10@gmail.com>
2022-10-09 20:08:58 +0100

    Added Stories

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,5 @@
 import HeroArea from "./utils/HeroArea";
+import FeaturesItem from "./utils/FeaturesItem";
 
 const Features = () => {
   return (
@@ -9,6 +10,40 @@ const Features = () => {
           text="We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories."
           image="/assets/features/desktop/hero"
         />
+      </section>
+      <section className="features-area">
+        <div className="l-container">
+          <FeaturesItem
+            image="responsive"
+            title="100% Responsive"
+            text="No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen."
+          />
+          <FeaturesItem
+            image="no-limit"
+            title="No Photo Upload Limit"
+            text="Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go."
+          />
+          <FeaturesItem
+            image="embed"
+            title="Available to Embed"
+            text="Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more. "
+          />
+          <FeaturesItem
+            image="custom-domain"
+            title="Custom Domain"
+            text="With Photosnap subscriptions you can host your stories on your own domain. You can also remove our branding! "
+          />
+          <FeaturesItem
+            image="boost-exposure"
+            title="Available to Embed"
+            text="Users that viewed your story or gallery can easily get notifed of new and featured stories with our built in mailing list. "
+          />
+          <FeaturesItem
+            image="drag-drop"
+            title="Available to Embed"
+            text="Easily drag and drop your image and get beautiful shots everytime. No over the top tooling to add friction to creating stories. "
+          />
+        </div>
       </section>
     </div>
   );
--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import StoriesHero from "./utils/StoriesHero";
+import StoriesItem from "./utils/StoriesItem";
 
 const Stories = props => {
   return (
@@ -17,6 +18,88 @@ const Stories = props => {
           date="March 2nd 2020"
           author="John Appleseed"
           linkText="Read the story"
+        />
+      </section>
+      <section className="stories-area">
+        <StoriesItem
+          image="mountains"
+          title="The Mountains"
+          author="by John Appleseed"
+        />
+        <StoriesItem
+          image="cityscapes"
+          title="Sunset Cityscapes"
+          author="by Benjamin Cruz"
+        />
+        <StoriesItem
+          image="18-days-voyage"
+          title="18 Days Voyage"
+          author="by Alexei Borodin"
+        />
+        <StoriesItem
+          image="architecturals"
+          title="Architecturals"
+          author="by Samantha Brooke"
+        />
+        <StoriesItem
+          image="world-tour"
+          title="World Tour 2019"
+          author="by Timothy Wagner"
+        />
+        <StoriesItem
+          image="unforeseen-corners"
+          title="Unforseen Corners"
+          author="by William Malcolm"
+        />
+        <StoriesItem
+          image="king-on-africa"
+          title="King on Africa: Part II"
+          author="by Tim Hillenburg"
+        />
+        <StoriesItem
+          image="trip-to-nowhere"
+          title="The Trip to Nowhere"
+          author="by Felicia Rourke"
+        />
+        <StoriesItem
+          image="rage-of-the-sea"
+          title="Rage of The Sea"
+          author="by Mohammed Abdul"
+        />
+        <StoriesItem
+          image="running-free"
+          title="Running Free"
+          author="by Michelle"
+        />
+        <StoriesItem
+          image="behind-the-waves"
+          title="Behind the Waves"
+          author="by Lamarr Wilson"
+        />
+        <StoriesItem
+          image="calm-waters"
+          title="Calm Waters"
+          author="by Samantha Brooke"
+        />
+        <StoriesItem
+          image="milky-way"
+          title="The Milky Way"
+          author="by Benjamin Cruz"
+        />
+        <StoriesItem
+          image="dark-forest"
+          title="Night at The Dark Forest"
+          author="by Mohammed Abdul"
+        />
+        <StoriesItem
+          image="somwarpet"
+          title="Somwarpet’s Beauty"
+          author="by Michelle"
+        />
+        <StoriesItem
+          image="land-of-dreams"
+          title="Land of Dreams"
+          author="by William Malcolm"
         />
       </section>
     </div>
